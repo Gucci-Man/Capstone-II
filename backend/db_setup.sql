3a588d27c2336a8be51b7e879ee329c2206041e8
mrtomno <adel.ngo@yahoo.com>
2024-04-14 17:52:49 -0500

    created test db

--- backend/db_setup.sql
+++ backend/db_setup.sql
@@ -7,3 +7,12 @@ CREATE DATABASE foodie_fit;
 
 \i schema_setup.sql
 \i seed.sql
+
+\echo 'Delete and recreate foodie_fit_test?'
+\prompt 'Return for yes or control-C to cancel >' foo
+
+DROP DATABASE foodie_fit_test;
+CREATE DATABASE foodie_fit_test;
+\connect foodie_fit_test
+
+\i schema_setup.sql
