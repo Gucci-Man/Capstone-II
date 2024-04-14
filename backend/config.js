9baa5fc9e271d255a1ebeef28f5a319f70cbec61
mrtomno <adel.ngo@yahoo.com>
2024-04-13 23:42:22 -0500

    hide secrets

--- backend/config.js
+++ backend/config.js
@@ -8,7 +8,7 @@ const DB = (process.env.NODE_ENV === "test")
     ? "foodie_fit_test"
     : "foodie_fit";
 
-const SECRET_KEY = process.env.SECRET_KEY || "fHt$TRcC%25HmE8gC'vD$!Ka%8sOt;~V,XItr+8t+)8;x}O/rSQgGyG7}o^c1')"
+const SECRET_KEY = process.env.SECRET_KEY 
 
 const BCRYPT_WORK_FACTOR = 12;
 
