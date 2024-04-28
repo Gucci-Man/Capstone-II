405e0e06f2412ac5a65684b683d69306c1b914d3
mrtomno <adel.ngo@yahoo.com>
2024-04-28 12:21:47 -0500

    start user profile

--- frontend/src/RouteList.jsx
+++ frontend/src/RouteList.jsx
@@ -4,6 +4,7 @@ import WelcomePage from './routes/WelcomePage';
 import RegistrationForm from './routes/RegistrationForm';
 import NotFound from './routes/NotFound';
 import Home from './routes/Home';
+import UserProfile from './routes/UserProfile';
 
 const RouteList = ({token}) => {
     /* console.log(`Inside RouteList: ${token}`) */
@@ -13,6 +14,7 @@ const RouteList = ({token}) => {
             <Route path="/register" element={<RegistrationForm />} />
             <Route path="/home" element={<Home token={token}/>} />
             <Route path="*" element={<NotFound />} />
+            <Route path="/user/:name" element={<UserProfile token={token}/>}/>
         </Routes>
     )
 };
---
+++ frontend/src/routes/UserProfile.jsx
@@ -0,0 +1,57 @@
+import React, { useState, useEffect } from 'react';
+import { useParams, useNavigate } from "react-router-dom";
+import axios from 'axios';
+
+const baseURL = import.meta.env.VITE_BASE_URL;
+
+const UserProfile = ({token}) => {
+    const { name } = useParams();
+    const [userData, setUserData] = useState(null);
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        // if token is not available, return to welcome page
+        if(!token && !localStorage.getItem('token')) {
+            navigate('/', {replace: true}); // prevent user from going to previous page
+        } 
+
+        const userToken = localStorage.getItem('token');
+        const fetchData = async () => {
+            setIsLoading(true);
+            try {
+                /* console.log(`token is ${userToken}`);
+                console.log(`name is ${name}`) */
+                const response = await axios.get(`${baseURL}/users/${name}`, {
+                    headers: {
+                        Authorization: `Bearer ${userToken}`
+                    }
+                });
+                console.log(response.data)
+                setUserData(response.data);
+            } catch (err) {
+                setError(err);
+            } finally {
+                setIsLoading(false);
+            }
+        };
+
+        fetchData();
+    }, [name]);
+
+    return (
+        <div>
+            {isLoading && <div>Loading user data...</div>}
+            {error && <div>Error fetching data: {error.message}</div>}
+            {userData && (
+                <div>
+                    <h2>{userData.user.username}</h2>
+                    <h4>{userData.user.email}</h4>
+                </div>
+            )}
+        </div>
+    );
+    
+}
+
+export default UserProfile;
